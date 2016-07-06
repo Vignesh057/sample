1d008f7575621cb0bc7c12845a02c2b9f2d01e69
Vignesh057 <vvicky057@gmail.com>
2016-07-06 11:43:16 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,12 @@
+public class PositiveOrNegative {
+
+    public void check(int number) {
+        if (number > 0) {
+            System.out.println(number + " is a positive number");
+        } else if (number < 0) {
+            System.out.println(number + " is a negative number");
+        } else {
+            System.out.println(number + " is neither positive nor negative");
+        }
+    }
+}
